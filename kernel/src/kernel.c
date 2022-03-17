bc578fdbc71038e7b206e9afc32c729505798aa2
Ian-Moffett <hscorp99906@gmail.com>
2022-03-17 16:34:42 -0400

    Got a start!

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,8 +1,11 @@
+#include "../stivale2.h"
 #include <stdint.h>
 #include <stddef.h>
-#include "../stivale2.h"
 
-/*
+
+static uint8_t stack[5000];
+
+
 void* get_tag(struct stivale2_struct* stivale2_struct, uint64_t id) {
     struct stivale2_tag* curTag = (void*)stivale2_struct->tags;
 
@@ -19,4 +22,30 @@ void* get_tag(struct stivale2_struct* stivale2_struct, uint64_t id) {
         curTag = (void*)curTag->next;
     }
 }
-*/
+
+
+static struct stivale2_header_tag_framebuffer lfb_tag = {
+    .tag = {
+        .identifier = STIVALE2_HEADER_TAG_FRAMEBUFFER_ID,
+        .next = 0
+    },
+
+    .framebuffer_width = 0,
+    .framebuffer_height = 0,
+    .framebuffer_bpp = 0
+};
+
+
+__attribute__((section(".stivale2hdr"), used)) static struct stivale2_header stivale_hdr = {
+    .entry_point = 0,
+    .stack = (uintptr_t)stack,
+    .flags = (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4),
+    .tags = (uintptr_t)&lfb_tag
+};
+
+
+void _start(struct stivale2_struct* stivale2_struct) {
+    while (1) {
+        __asm__ __volatile__("hlt");
+    }
+}
